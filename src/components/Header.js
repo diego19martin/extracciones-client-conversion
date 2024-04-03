8f29fe879ffd8cdb12f3ec44bee8203da5dd2f8a
Diego Argonz <diego19martin@hotmail.com>
2024-04-03 15:43:32 -0300

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <header className='headerConv'>Sistema de extracciones</header>
+    <header className='headerConv'>Sistema de extracciones-anticipadas</header>
   )
 }
